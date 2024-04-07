6272476cb8e8ade156065a01c2f5489ebff8eabf
Shimaa-78 <90785340+Shimaa-78@users.noreply.github.com>
2024-04-07 20:58:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
  
 </head>
@@ -107,7 +107,7 @@
         <?php include("footer.php"); ?>
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="js/index.js"></script>
+    <script src="main.js"></script>
     
 </body>
 </html>
